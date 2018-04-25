0c391d71d25347804a358f05f5ef8855c162ee56
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-04-25 06:42:36 +0200

    Document the vector module
    this also includes a series of tests

--- eschers/src/vector/mod.rs
+++ eschers/src/vector/mod.rs
@@ -1,8 +1,70 @@
 //! 2D vectors and associated functions
+//!
+//! Vectors are used to represent locations in the two dimensional plane.
+//!
+//! Vectors can be added.
+//!
+//! ```
+//! # use eschers::vector::Vector;
+//! let u = Vector::new(1f64, 0f64);
+//! let v = Vector::new(0f64, 1f64);
+//!
+//! let w = u.add(&v);
+//!
+//! assert_eq!(w, Vector::new(1f64, 1f64));
+//! ```
+//!
+//! You can negate a vector.
+//!
+//! ```
+//! # use eschers::vector::Vector;
+//! # let u = Vector::new(1f64, 0f64);
+//!
+//! let w = u.neg();
+//!
+//! assert_eq!(w, Vector::new(-1f64, 0f64));
+//! ```
+//!
+//! Subtraction of two vectors is also an option.
+//!
+//! ```
+//! # use eschers::vector::Vector;
+//! # let u = Vector::new(1f64, 0f64);
+//! # let v = Vector::new(0f64, 1f64);
+//!
+//! let w = u.sub(&v);
+//!
+//! assert_eq!(w, Vector::new(1f64, -1f64));
+//! ```
+//!
+//! Just as scaling a vector.
+//!
+//! ```
+//! # use eschers::vector::Vector;
+//! # let u = Vector::new(1f64, 0f64);
+//!
+//! let w = u.scale(&2f64);
+//!
+//! assert_eq!(w, Vector::new(2f64, 0f64));
+//! ```
+//!
+//! Finally with a `Vector<f64>` you can determine their length.
+//!
+//! ```
+//! # use eschers::vector::Vector;
+//! let t = Vector::new(3f64, 4f64);
+//!
+//! assert_eq!(5f64, t.length());
+//! ```
+//!
+//! Note that all the methods that return a `Vector`, do so by returning a new
+//! structure.
+
 
 use std::ops::{Add, Neg, Mul};
 
 /// Representation of two dimensional vector
+#[derive(Debug, PartialEq, Eq)]
 pub struct Vector<T> {
     x: T,
     y: T,
