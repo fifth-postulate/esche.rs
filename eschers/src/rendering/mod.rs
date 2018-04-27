70654e7e0503854dabbf0a615993e694da9a4d61
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-04-27 13:25:07 +0200

    Map a Line structure to an svg element

--- eschers/src/rendering/mod.rs
+++ eschers/src/rendering/mod.rs
@@ -1,12 +1,39 @@
 //! Turning a `Rendering` into an SVG.
 
 use svg::Document;
+use svg::node::Node;
+use svg::node::element as Svg;
+
+use shape::Shape;
 use picture::Rendering;
 
 type Bounds = (f64, f64);
 
 /// Create an SVG document from a `Rendering`
-pub fn to_svg(bounds: Bounds, _rendering: &Rendering) -> Document {
-    Document::new()
-        .set("viewbox", (0.0, 0.0, bounds.0, bounds.1))
+pub fn to_svg(bounds: Bounds, rendering: &Rendering) -> Document {
+    let mut document = Document::new()
+        .set("viewbox", (0.0, 0.0, bounds.0, bounds.1));
+
+    for (shape, style) in rendering {
+        let mut node = to_svg_node(&shape);
+        node.assign("stroke", "black");
+        node.assign("stroke-width", style.stroke_width);
+        node.assign("fill", "none");
+        document.append(node);
+    }
+
+    document
+}
+
+fn to_svg_node(shape: &Shape) -> impl Node {
+    match shape {
+        Shape::Line(line_start, line_end) => {
+            Svg::Line::new()
+                .set("x1", line_start.x)
+                .set("y1", line_start.y)
+                .set("x2", line_end.x)
+                .set("y2", line_end.y)
+        },
+        _ => panic!(),
+    }
 }
