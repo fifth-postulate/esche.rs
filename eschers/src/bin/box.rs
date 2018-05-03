f98240f2840c851f0b9fbee55d0a50f035094431
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-05-03 10:51:43 +0200

    Create a box printer

---
+++ eschers/src/bin/box.rs
@@ -0,0 +1,16 @@
+extern crate svg;
+extern crate eschers;
+
+use eschers::vector::Vector;
+use eschers::canvas::Box as Bx;
+use eschers::rendering::canvas::to_svg as box_to_svg;
+
+fn main() {
+    let bx = Bx::new(
+        Vector::new(75f64, 75f64),
+        Vector::new(250f64, 0f64),
+        Vector::new(0f64, 250f64)
+    );
+    let document = box_to_svg((400f64, 400f64), &bx);
+    svg::save("output.svg", &document).unwrap();
+}
